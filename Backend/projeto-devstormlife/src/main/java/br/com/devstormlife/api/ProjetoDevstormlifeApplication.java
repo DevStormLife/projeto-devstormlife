6bbd7151c52cffb0ed20c0a3ad19e6eacaa4a310
VgBraga <dev.victor.nascimento@gmail.com>
2026-03-20 10:17:35 -0300

    commit 1

---
+++ Backend/projeto-devstormlife/src/main/java/br/com/devstormlife/api/ProjetoDevstormlifeApplication.java
@@ -0,0 +1,13 @@
+package br.com.devstormlife.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDevstormlifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDevstormlifeApplication.class, args);
+	}
+
+}
