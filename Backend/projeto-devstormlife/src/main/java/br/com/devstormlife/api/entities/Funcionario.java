c3089e13eac2086977e3788f118d06bdd641bc41
VgBraga <dev.victor.nascimento@gmail.com>
2026-03-20 10:42:10 -0300

    commit 3

--- Backend/projeto-devstormlife/src/main/java/br/com/devstormlife/api/entities/Funcionario.java
+++ Backend/projeto-devstormlife/src/main/java/br/com/devstormlife/api/entities/Funcionario.java
@@ -1,5 +1,18 @@
 package br.com.devstormlife.api.entities;
 
-public class Funcionario {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "tb_funcionarios")
+public class Funcionario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	
 }
