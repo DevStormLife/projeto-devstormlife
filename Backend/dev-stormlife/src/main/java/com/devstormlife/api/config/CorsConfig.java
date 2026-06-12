851407749aef45be13cf6e1f3bed39deac712b43
VgBraga <dev.victor.nascimento@gmail.com>
2026-06-12 13:24:53 -0300

    alteração do cors config na esperança de resolver o erro 403

--- Backend/dev-stormlife/src/main/java/com/devstormlife/api/config/CorsConfig.java
+++ Backend/dev-stormlife/src/main/java/com/devstormlife/api/config/CorsConfig.java
@@ -24,12 +24,12 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         
         // Liberação explícita de acesso endpoint por endpoint
-        source.registerCorsConfiguration("/funcionarios", config);
-        source.registerCorsConfiguration("/usuarios", config);
-        source.registerCorsConfiguration("/usuarios/login", config);
-        source.registerCorsConfiguration("/usuarios/cadastro", config);
-        source.registerCorsConfiguration("/cartao", config);
-        source.registerCorsConfiguration("/subestacoes", config);
+        source.registerCorsConfiguration("/funcionarios/**", config);
+        source.registerCorsConfiguration("/usuarios/**", config);
+        source.registerCorsConfiguration("/usuarios/login/**", config);
+        source.registerCorsConfiguration("/usuarios/cadastro/**", config);
+        source.registerCorsConfiguration("/cartao/**", config);
+        source.registerCorsConfiguration("/subestacoes/**", config);
 
         // Retorna a configuração estruturada para o ecossistema do Spring
         return new CorsFilter(source);
