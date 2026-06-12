b18cf1e04a3f47b01435184bed26e6706b367e89
ghmelos <gdemoraismelos@gmail.com>
2026-06-12 07:55:19 -0300

    alteração cadastro.js

--- Frontend/script/cadastro.js
+++ Frontend/script/cadastro.js
@@ -6,36 +6,33 @@ document.addEventListener('DOMContentLoaded', () => {
     cadastroForm.addEventListener('submit', (event) => {
         event.preventDefault();
 
-        // captura dos campos
         const inputs = document.querySelectorAll('.login-input');
         const nome = inputs[0].value.trim();
         const cpf = inputs[1].value.trim();
         const email = inputs[2].value.trim();
         const senha = inputs[3].value.trim();
 
-        // validação de campos vazios
         if (!nome, !cpf, !email, !senha) {
             alert('Por favor, preencha todos os campos do cadastro.');
             return;
         }
 
-        // validação básica do CPF
+        // validação do CPF
         if (cpf.length < 11) {
             alert('Por favor, insira um CPF válido.');
             return;
         }
 
-        // validação de senha
+        // validação da senha
         if (senha.length < 8) {
             alert('A senha deve ter no mínimo 8 caracteres.');
             return;
         }
 
-        // --- SIMULAÇÃO DE SALVAMENTO ---
         console.log('Dados cadastrados:', { nome, cpf, email, senha });
 
         alert('Cadastro realizado com sucesso!');
 
-        window.location.href = 'pgInicial.html';
+        window.location.href = '../index.html';
     });
 });
